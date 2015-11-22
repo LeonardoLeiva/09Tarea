e0fb6a1a5fae58579e7b7f47f00f2f3a06b382f7
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-21 22:15:07 -0400

    inicializacion p1

---
+++ parte1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Este codigo aproxima la constante de Hubble con los primeros datos
+experimentales. Se espera que no sea suficientemente cercano al valor
+real
+'''
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+# funciones estructurales
+
+
+def leer_archivo():
+    '''
+    lee el archivo
+    '''
+    pass
+
+
+def aproximar_parametros(a):
+    '''
+    aproxima los parametos a. puede ser un escalar o un conjunto de escalares
+    a optimizar
+    '''
+    pass
+
+
+# main
+
+
+# graficos
