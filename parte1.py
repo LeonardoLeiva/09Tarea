abd5cbf4f575340b3456aed44798af707971aef9
LeonardoLeiva <leonardo_conan@hotmail.com>
2015-11-21 23:50:32 -0400

    aprox inicial de H_0
    falta ajustar unidades de medida
    falta buscar una adivinanza sobre el valor de H_0. El resultado no
    deberia cambiar por ser una funcion lineal que no tiene otro minimo
    posible
    falta hacer el calculo de intervalos de confianza

--- parte1.py
+++ parte1.py
@@ -10,14 +10,18 @@ real
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import leastsq
 
 # funciones estructurales
 
 
-def leer_archivo():
+def leer_archivo(nombre):
     '''
     lee el archivo
+    nombre debe ser un str
     '''
+    datos = np.loadtxt(nombre)
+    return datos
     pass
 
 
@@ -29,7 +33,42 @@ def aproximar_parametros(a):
     pass
 
 
+def y_aprox(x, param):
+    '''
+    '''
+    a = param
+    b = 0
+    y = a * x + b
+    return y
+
+
+def residuos(param, x, y):
+    c = param
+    y_ap = y_aprox(x, param)
+    err = y - y_ap
+    return err
+
+
 # main
-
-
+nom = "data/hubble_original.dat"
+datos = leer_archivo(nom)
+d = datos[:, 0]
+v = datos[:, 1]
+adivinanza = 500
+aprox = leastsq(residuos, adivinanza, args=(d, v))
+# datos para graficar
+d_aprox = np.linspace(d[0], d[-1], 10)
+H_0 = aprox[0]
+print H_0
+v_aprox = y_aprox(d_aprox, H_0)
 # graficos
+fig = plt.figure()
+fig.clf()
+ax1 = fig.add_subplot(111)
+ax1.plot(d, v, 'o')
+ax1.plot(d_aprox, v_aprox)
+ax1.set_xlabel("d")
+ax1.set_ylabel("v(d)")
+plt.savefig("parte1.png")
+plt.draw()
+plt.show()
